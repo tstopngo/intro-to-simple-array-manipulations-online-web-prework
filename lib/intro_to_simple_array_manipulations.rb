e82e3040eb7aae23d63a6f5412b2108dd31af6bc
Tracy Ngo <tngo1206@gmail.com>
2019-02-07 20:22:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,21 @@ def using_pop (array)
 end
 
 def pop_with_args(array)
-  array.pop
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+def using_insert (array, element)
+  array.insert(element[3])
+end
